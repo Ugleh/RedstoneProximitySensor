f448e5c12f8bb2ea1b755c228f424050368517d4
Ugleh <troti@ymail.com>
2018-02-06 12:52:33 -0500

    Modified paramaters to take RPS instead of Loc & ID

--- src/com/ugleh/redstoneproximitysensor/addons/AddonTemplate.java
+++ src/com/ugleh/redstoneproximitysensor/addons/AddonTemplate.java
@@ -1,15 +1,13 @@
 package com.ugleh.redstoneproximitysensor.addons;
 
-import java.util.List;
-import java.util.UUID;
-
-import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 
 import com.ugleh.redstoneproximitysensor.utils.RPS;
 
 public abstract class AddonTemplate {
-	public abstract boolean checkTrigger(List<String> acceptedEntities, Entity e, Location l, UUID ownerID);
+	public abstract boolean checkTrigger(RPS rps, Entity e);
 	public abstract void buttonPressed(Boolean on, RPS affectedRPS);
+	public abstract void rpsCreated(RPS affectedRPS);
+	public abstract void rpsRemoved(RPS affectedRPS);
 	
 }
