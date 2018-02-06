9ad715dd242ab28fd6f1cb95eb8a8e1627225994
Ugleh <troti@ymail.com>
2018-02-06 12:54:17 -0500

    Changed how the trigger is set off

--- src/com/ugleh/redstoneproximitysensor/addons/LegacyFactionsAddon.java
+++ src/com/ugleh/redstoneproximitysensor/addons/LegacyFactionsAddon.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -13,19 +12,22 @@ import org.bukkit.inventory.ItemStack;
 import com.ugleh.redstoneproximitysensor.RedstoneProximitySensor;
 import com.ugleh.redstoneproximitysensor.listeners.PlayerListener;
 import com.ugleh.redstoneproximitysensor.utils.RPS;
-import com.ugleh.redstoneproximitysensor.utils.Trigger; 
+import com.ugleh.redstoneproximitysensor.utils.Trigger;
 
+import net.redstoneore.legacyfactions.entity.FPlayer;
+import net.redstoneore.legacyfactions.entity.FPlayerColl;
 import net.redstoneore.legacyfactions.entity.Faction;
 import net.redstoneore.legacyfactions.entity.FactionColl;
  
 public class LegacyFactionsAddon extends AddonTemplate{
 	public String flagName = "LEGACYFACTIONS";
+    //private HashMap<String, String> sensorFactions = new HashMap<String, String>();
 	public LegacyFactionsAddon()
 	{
-		createButton();
+		CreateButton();
 	}
 	
-	private void createButton()
+	private void CreateButton()
 	{
 		List<String> lore = pl().WordWrapLore(pl().langString("lang_button_lf_lore"));
 		pl().addTrigger(new Trigger("button_lfactiontrigger", new ItemStack(Material.FENCE), "lang_button_lftrigger", flagName, "lang_button_true", "lang_button_false", lore, this));
@@ -42,11 +44,13 @@ public class LegacyFactionsAddon extends AddonTemplate{
 	}
 
 	@Override
-	public boolean checkTrigger(List<String> acceptedEntities, Entity e, Location l, UUID ownerID) {
-		if(!acceptedEntities.contains(flagName))return false;
+	public boolean checkTrigger(RPS rps, Entity e) {
+		UUID ownerID = rps.getOwner();
+		if(!rps.getAcceptedEntities().contains(flagName))return false;
 		if(!(e instanceof Player)) return false;
 		Faction faction = FactionColl.get(Bukkit.getOfflinePlayer(ownerID));
-		if(faction.getOnlinePlayers().contains((Player)e))
+		FPlayer fplayer = FPlayerColl.get((Player)e);
+		if(faction.getFPlayers().contains(fplayer))
 			return true;
 		return false;
 		
@@ -54,6 +58,15 @@ public class LegacyFactionsAddon extends AddonTemplate{
 
 	@Override
 	public void buttonPressed(Boolean on, RPS affectedRPS) {
+	}
+
+	@Override
+	public void rpsCreated(RPS affectedRPS) {
+	}
+
+	@Override
+	public void rpsRemoved(RPS affectedRPS) {
+
 	}
 	
 }
